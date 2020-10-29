7d86ffe80dc376739917b8d0c2064df0f35b7041
Sam Cutler <sam.cutler@gmail.com>
2020-10-29 14:46:45 -0700

    chunk of progress

--- app.js
+++ app.js
@@ -108,21 +108,13 @@ let store = {
 // Create the string that will "summarize" quiz progress at the top.
 function summaryHTMLaddition() {
   console.log('Ran summaryHTMLaddition function.');
-  
-  // If quiz hasn't started OR if it's done, this should be empty...
-  if (store.quizStarted === false || store.quizCompleted === true) {
-    return '';
-  
-  // ...otherwise, provide the summary of progress.
-  } else {
-    return `
-      <section class="major-section">
-        <header>
-          <h3>You're on question ${store.questionNumber} out of 5.</h3>
-          <h3>Current score is ${store.score} points.</h3>
-        </header>
-      </section>`
-  };
+  return `
+    <section class="major-section">
+      <header>
+        <h3>You're on question ${store.questionNumber} out of 5.</h3>
+        <h3>Current score is ${store.score} points.</h3>
+      </header>
+    </section>`;
 }
 
 // Create the string that will "welcome" users to the start of the quiz.
@@ -137,71 +129,88 @@ function quizStartHTML() {
     </div>`;
 }
 
+// Create the HTML string that begins every question core content
+function QandAstringStart() {
+  
+  // Identify the current question object in the store
+  const currentQuestion = store.questions[(store.questionNumber-1)];
+
+  return `
+    <section class="major-section">
+      <h3>${currentQuestion.question}</h3>  
+      <form>
+        <div class="answer-choices-container">`;
+}
+  
 // Create a string containing each answer choice
 function makeAnswerChoiceHTMLstring(answerNumber) {
-  console.log('makeAnswerChoiceHTMLstring function.');
+  console.log('Ran makeAnswerChoiceHTMLstring function.');
 
+  // Identify the current question object in the store
   const currentQuestion = store.questions[(store.questionNumber-1)];
 
   let answerChoiceString = `
-    <input type="radio" name='${currentQuestion.questionShorthand}' value='${currentQuestion.answerVals[answerNumber]}' required>
+    <input type="radio" id="option${answerNumber}" value="${currentQuestion.answerVals[answerNumber]}" name="options" required>
       <label for="${currentQuestion.answerVals[answerNumber]}">${currentQuestion.answers[answerNumber]}</label><br>`
+    
+  /*
+  let answerChoiceString = `
+    <input type="radio" id="${currentQuestion.answerVals[answerNumber]}" name='${currentQuestion.questionShorthand}' value='${currentQuestion.answerVals[answerNumber]}' required>
+      <label for="${currentQuestion.answerVals[answerNumber]}">${currentQuestion.answers[answerNumber]}</label><br>`
+  */
+
   return answerChoiceString;
 
 }
 
-// Create the string containing the current question and answer choices. 
-function quizQuestionHTML() {
-  console.log('The quizQuestionHTML function ran.');
+// Create a string to conclude QandA HTML, including a "Submit Answer" button.
+function provideSubmitAnswerButtonString() {
+  return `
+    <button type="submit">Submit selection.</button>
+    </div>
+    </form>
+    </section>`;
+}
 
-  const currentQuestion = store.questions[(store.questionNumber-1)];
-  console.log('Determined current question.')
-  
-  // Create the string with the question and answer choices, as the start of a form.
-  let QandAstring = `
-    <section class="major-section">
-      <form>
-        <h3 class="js-test-click">${currentQuestion.question}</h3>
-        <div class="answer-choices-container">`;
-  console.log('Started the QandAstring.')
+// Create a string to conclude QandA HTML, including a "See quiz results" button.
+function provideSeeResultsButtonString() {
+  return ` 
+    </div>
+    </form>
+    <button class='js-see-results-button'>See quiz results.</button>
+    </section>`;
+}
 
-  // Add each answer choice
-  let i = '';
-  for (i=0; i<4; i++) {
-    //console.log(i);
-    let answerChoiceAddition = makeAnswerChoiceHTMLstring(i);
-    QandAstring += answerChoiceAddition;
-  };
+// Create a string to conclude QandA HTML, including a "Next question" button. 
+function provideNextQuestionButtonString() { 
+  return ` 
+    </form>
+    <button class='js-next-question-button'>Next question.</button>
+    </section>`; 
+}
 
-  // A few different ways to end this string (always includes ending the answer-choices <div>)...
-  let QandAstringFinish = '</div>';
 
-  // ...if the active question hasn't yet been answered, need a "Submit answer" button.
-  if (store.questionAnswered === 'empty') {
-    QandAstringFinish = 
-        ` <button class='js-submit-answer-button'>Submit answer.</button>
-          </form>
-          </section>`;
-  
-  // ...if the 5th, and thus final, question has been answered, need a "See quiz results" button.
-  } else if (store.questionNumber === 5 && store.questionAnswered != 'empty') {
-    QandAstringFinish = 
-        ` </form>
-          <button class='js-see-results-button'>See quiz results.</button>
-          </section>`; 
-  
-  // ...otherwise, need a "Next question" button. 
-  } else {
-    QandAstringFinish = 
-        `</form>
-         <button class='js-next-question-button'>Next question.</button>
-         </section>`; 
-  };
+// Troubleshooting my radio button form requirement...
+function renderQuizQuestionHTMLtester() {
+  // The following form WORKS (is required!) in my quiz. So, seek to emulate it!
+  return `
+  <section class="major-section">
+    <h3>Here is the question.</h3>
+    <form>
+      <div class="answer-choices-container">
+        <input type="radio" id="pizza" value="pizza" name="favfood" required>
+          <label for="pizza">Pizza</label><br>
+        <input type="radio" id="hotdog" value="hotdog" name="favfood" required>
+          <label for="hotdog">Hot dogs</label><br>
+        <input type="radio" id="hamburger" value="hamburger" name="favfood" required>
+          <label for="hamburger">Hamburger</label><br>
 
-  // Append the "finish" to the existing string, and return it. 
-  QandAstring += QandAstringFinish;
-  return QandAstring;
-  
+        <button type="submit">Submit selection.</button>
+      </div>
+    </form>
+  </section>
+  `
+
 }
 
 // Create the string containing the "end of quiz" message.
@@ -214,34 +223,6 @@ function quizCompleteHTML() {
       <p>You may take this quiz as many times as you like. To restart, please click the button below.</p>
       <button class='js-restart-button'>Restart quiz.</button>
     </section>`;
-}
-
-// Depending on the position in the quiz, will use one of 3 major 
-// "core content" functions:
-function coreContentHTMLaddition() {
-  console.log('Ran coreContentHTMLaddition function.');
-  
-  // initialize empty coreContentString to be filled out
-  let coreContentString = ''
-
-  // Go to the "quiz start" form function:
-  if (store.quizStarted === false) {
-    console.log('The quiz is starting.');
-    coreContentString = quizStartHTML();
-
-  // Go to "quiz conclusion" form function:
-  } else if (store.quizCompleted === true) {
-    console.log('The quiz has finished, need to provide summary');
-    coreContentString = quizCompleteHTML();
-  
-  // Otherwise, go to the "quiz question" form function:
-  } else {
-    console.log('The quiz has already started');
-    return quizQuestionHTML();
-  };
-
-  return coreContentString;
-
 }
 
 // Want to provide right/wrong feedback after answers are submitted.
@@ -261,7 +242,9 @@ function feedbackHTMLaddition() {
   // Feedback for an incorrect answer, including the correct answer:
   } else if (store.questionAnswered === 'incorrect') {
     console.log('A question has been answered incorrectly.');
-    const currentQuestion = store.questions[(store.questionNumber-1)]
+    
+    // Identify the current question object in the store
+    const currentQuestion = store.questions[(store.questionNumber-1)];
     return `<section class="major-section"><p class="need-space">That is incorrect. The correct answer is "${currentQuestion.correctAnswerVerbose}"</p></section>`;
   };
   
@@ -270,8 +253,100 @@ function feedbackHTMLaddition() {
 
 /********** RENDER FUNCTION(S) **********/
 
-// This function conditionally replaces the contents of the <main> tag based on the state of the store
+// Add additional render functions with "logic", each one using template generation functions
 
+// Conditions governing rendering the summary information:
+function renderSummaryHTMLaddition() {
+  console.log('Ran renderSummaryHTMLaddition function.')
+  
+  // If quiz hasn't started OR if it's done, this should be empty...
+  if (store.quizStarted === false || store.quizCompleted === true) {
+    return '';
+  
+  // ...otherwise, provide the summary of progress.
+  } else {
+    return summaryHTMLaddition();
+  };
+}
+
+// Conditions governing how to finish the question content
+function renderQandAstringFinish() {
+  console.log('Ran renderQandAstringFinish function.')
+
+  // A few different ways to end this string, but 
+  // always includes ending the answer-choices <div> (does it???)
+  let QandAstringFinish = '';
+
+  // ...if the active question hasn't yet been answered, need a "Submit answer" button.
+  if (store.questionAnswered === 'empty') {
+    QandAstringFinish = provideSubmitAnswerButtonString();
+  
+  // ...if the 5th, and thus final, question has been answered, need a "See quiz results" button.
+  } else if (store.questionNumber === 5 && store.questionAnswered != 'empty') {
+    QandAstringFinish = provideSeeResultsButtonString();
+  
+  // ...otherwise, need a "Next question" button. 
+  } else {
+    QandAstringFinish = provideNextQuestionButtonString(); 
+  };
+
+  return QandAstringFinish;
+}
+
+// Conditions governing the quiz question and answer choices
+function renderQuizQuestionHTML() {
+  console.log('Ran renderQuizQuestionHTML function.');
+
+  // Identify the current question object in the store
+  const currentQuestion = store.questions[(store.questionNumber-1)];
+  
+  // Start the string with the question and answer choices, as the start of a form.
+  let QandAstring = QandAstringStart();
+
+  // Add each answer choice
+  let i = '';
+  for (i=0; i<4; i++) {
+    let answerChoiceAddition = makeAnswerChoiceHTMLstring(i);
+    QandAstring += answerChoiceAddition;
+  };
+
+  // Add the conclusion of the string, with appropriately located/labeled button.
+  let QandAstringFinish = renderQandAstringFinish();
+  QandAstring += QandAstringFinish;
+  
+  return QandAstring;
+}
+
+// Conditions governing rendering the core content:
+function renderCoreContentHTMLaddition() {
+  console.log('Ran renderCoreContentHTMLaddition function');
+
+  // initialize empty coreContentString to be filled out
+  let coreContentString = ''
+
+  // Go to the "quiz start" form function:
+  if (store.quizStarted === false) {
+    console.log('The quiz is starting.');
+    coreContentString = quizStartHTML();
+
+  // Go to "quiz conclusion" form function:
+  } else if (store.quizCompleted === true) {
+    console.log('The quiz has finished, need to provide summary');
+    coreContentString = quizCompleteHTML();
+  
+  // Otherwise, go to the "quiz question" form function:
+  } else {
+    console.log('The quiz has already started');
+    return renderQuizQuestionHTML();
+    //return quizQuestionHTMLtester();
+  };
+
+  return coreContentString;
+
+}
+
+
+// This function conditionally replaces the contents of the <main> tag based on the state of the store
 function renderQuizPage() {
   console.log('Ran renderQuizPage function.');
   
@@ -279,10 +354,10 @@ function renderQuizPage() {
   let HTMLstring = '';
 
   // Generate the string/HTML content for the "Summary"
-  HTMLstring = summaryHTMLaddition();
+  HTMLstring = renderSummaryHTMLaddition();
   
   // Generate the string/HTML content for the question + answer choices
-  HTMLstring += coreContentHTMLaddition();
+  HTMLstring += renderCoreContentHTMLaddition();
 
   // Generate the string/HTML content for the feedback + button
   HTMLstring += feedbackHTMLaddition();
@@ -295,7 +370,6 @@ function renderQuizPage() {
 /********** EVENT HANDLER FUNCTIONS **********/
 
 // These functions handle events (submit, click, etc)
-
 // This page will need to handle...
 
 // ...clicks of the "start quiz" button
@@ -313,6 +387,7 @@ function handleStartQuiz() {
     store.questionNumber = 1;
     console.log('Setting "questionNumber" to "1".')
 
+    // Re-render the DOM.
     console.log('With those changes in place, re-rendering the DOM.')
     renderQuizPage();
 
@@ -350,6 +425,7 @@ function handleAnswerSubmission() {
       console.log('store.questionAnswered is currently set to '+store.questionAnswered);
     };
 
+    // Re-render the DOM.
     renderQuizPage();
 
   });
@@ -382,6 +458,8 @@ function handleSeeResults() {
   $('.js-core-HTML-target').on('click','.js-see-results-button', function(event) {
     console.log('You clicked the "see results" button.');
     store.quizCompleted = true;
+
+    // Re-render the DOM.
     renderQuizPage();
   })
 }
